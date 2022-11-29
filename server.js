7dc1844671f33962ac02edac243e7878cce94e1f
Dmitri Kou <yaya3372@yandex.ru>
2022-11-29 19:33:14 +0300

    review fixes

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/dist'));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/dist/index.html'))
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
